7d172658e1ee6b7e4b8769b63cf36482fb7fd0bb
alec gheeraert <gheeraert.alec@gmail.com>
2021-11-23 10:25:09 +0100

    start spotify api

--- script/app.js
+++ script/app.js
@@ -1,3 +1,35 @@
 var client_id = '6247141433f742069af737fa75514817';
 var client_secret = '0b46b8949b8643e5837d84f486e266cc';
-var redirect_uri = 'http://localhost:5500';
+var redirect_uri = 'http://127.0.0.1:5500/index.html';
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
+
+function requestAuthorization(){    // Temporary
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url;
+}
+
+function onPageLoad() {
+    if (window.location.search.length > 0) {
+        handleRedirect();
+    }
+}
+
+function handleRedirect() {
+    getCode();
+}
+
+function getCode(){
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+}
