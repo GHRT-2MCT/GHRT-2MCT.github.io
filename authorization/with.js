655aec5ce9cfe7fe9dc41f7bb23e8766aa0539a5
alec gheeraert <gheeraert.alec@gmail.com>
2021-11-25 13:49:08 +0100

    search genre for recommend

--- authorization/with.js
+++ authorization/with.js
@@ -1,20 +1,70 @@
+/* 
+    id 6247141433f742069af737fa75514817
+    se 0b46b8949b8643e5837d84f486e266cc
+
+    To do
+    Input for client id and secret
+*/ 
+
+
+
 var client_id = '6247141433f742069af737fa75514817';
 var client_secret = '0b46b8949b8643e5837d84f486e266cc';
 var redirect_uri = 'http://127.0.0.1:5500/index.html';
 
 var access_token = '';
-var refresh_token = 'AQA2vs06DOpXDAXvxX1f1Ax3WupRvNOLYVMQSCMvtDaAfc4lPkjuHV3Ey5Fz9Fj9De-V4Zdtz_tJt4LhuTP01BhloGuaJ4xxlNxvM2L6T5VMZ5AOTC1d5BfCGLK4dg1ZSTI';
+var refresh_token = '';
 
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
 const TOKEN = "https://accounts.spotify.com/api/token";
 const RECOMMENDATION = 'https://api.spotify.com/v1/recommendations';
 const GENRES = 'https://api.spotify.com/v1/recommendations/available-genre-seeds';
 
-var genres_arr;
-
-function onPageLoad() {
-    refreshAccessToken();
+/*
+    Authorization request
+*/
+function requestAuthorization() {   // Return redirect URI with code
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url;
 }
 
+
+/*
+    Handling redirect
+*/
+function onPageLoad() {
+    if (window.location.search.length > 0) {
+        handleRedirect();
+    }
+}
+function handleRedirect() {
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ) {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code');
+    }
+    fetchAccessToken(code);
+    window.history.pushState("", "", redirect_uri);
+}
+
+
+/*
+    Create body for fetching access_token
+*/  
+function fetchAccessToken( code ) {
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
 function refreshAccessToken() {
     let body = "grant_type=refresh_token";
     body += "&refresh_token=" + refresh_token;
@@ -37,13 +87,17 @@ function callAuthorizationApi(body) {
 function handleAuthorizationResponse() {
     if ( this.status == 200 ) {
         var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
         if ( data.access_token != undefined ) {
             access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
         }
         if ( data.refresh_token  != undefined ) {
             refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
         }
-        requestGenres();
+        onPageLoad();
     } else {
         console.log(this.responseText);
         alert(this.responseText);
@@ -64,16 +118,17 @@ function callApi(method, url, body, callback) {
 }
 // Genres
 function requestGenres() {
+    access_token = localStorage.getItem("access_token");
     if ( access_token != null ) {
         callApi( "GET", GENRES, null, handleGenresResponse );
     } else {
         alert('Authorization needed');
     }
 }
-function handleGenresResponse() {
+function handleGenresResponse () {
     if ( this.status == 200 ) {
-        var genres = JSON.parse(this.responseText);
-        placeSearchGenres(genres);
+        var data = JSON.parse(this.responseText);
+        console.log(data);
     } else if ( this.status == 401 ) {
         refreshAccessToken()
     } else {
@@ -83,49 +138,22 @@ function handleGenresResponse() {
 }
 
 // Recommendations
-function requestRecommendation(genre) {
-    if ( access_token != null && genre != null) {
-        callApi( "GET", RECOMMENDATION  + "?limit=1&seed_genres=" + genre, null, handleRecommendationResponse );
+function requestRecommendation() {
+    access_token = localStorage.getItem("access_token");
+    if ( access_token != null ) {
+        callApi( "GET", RECOMMENDATION  + "?limit=1&seed_genres=pop", null, handleRecommendationResponse );
     } else {
         alert('Authorization needed');
     }
 }
-function handleRecommendationResponse() {
+function handleRecommendationResponse () {
     if ( this.status == 200 ) {
-        var recommendation = JSON.parse(this.responseText);
-        console.log(recommendation);
+        var data = JSON.parse(this.responseText);
+        console.log(data);
     } else if ( this.status == 401 ) {
         refreshAccessToken()
     } else {
         console.log(this.responseText);
         alert(this.responseText);
     }
-}
-
-
-/*
-    Genres search
-*/
-function placeSearchGenres(genres) {
-    genres_arr = genres.genres.slice();
-}
-
-function searchGenres() {
-    var input = document.getElementById('searchGenres');
-    var filter = input.value.toUpperCase();
-
-    for (i = 0; i < genres_arr.length; i++) {
-        genre = genres_arr[i];
-        if (genre.toUpperCase().indexOf(filter) > -1) {
-            document.getElementById('searchLI').innerHTML = genre;
-            break;
-        } else {
-            document.getElementById('searchLI').innerHTML = '';
-        }
-      }
-}
-
-function searchLI() {
-    var genre = document.getElementById('searchLI').innerHTML;
-    requestRecommendation(genre);
 }
